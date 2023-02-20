074e40cf996c263215b51443a459ce225b3ad7e4
Sarthak Shyngle <50234097+Sarthak160@users.noreply.github.com>
2023-02-20 16:44:21 +0530

    feat: integrate mock library

--- src/main/java/org/example/HttpCall.java
+++ src/main/java/org/example/HttpCall.java
@@ -5,17 +5,53 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class HttpCall {
-    public static void main(String[] args) throws Exception {
+    public HttpCall() {
+
+    }
+
+    public String getResponse2() throws Exception {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://google.com")
+                .get()
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            int responseCode = response.code();
+            System.out.println("Response Code : " + responseCode);
+            assert response.body() != null;
+            String responseBody = response.body().string();
+            System.out.println(responseBody);
+            return responseBody;
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
+        return "";
+    }
+
+    public String getResponse() throws Exception {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url("http://example.com")
                 .get()
                 .build();
+
         try (Response response = client.newCall(request).execute()) {
             int responseCode = response.code();
             System.out.println("Response Code : " + responseCode);
+            assert response.body() != null;
             String responseBody = response.body().string();
             System.out.println(responseBody);
+            return responseBody;
+        } catch (Exception exception) {
+            System.out.println(exception);
         }
+        return "";
+    }
+
+    public static void main(String[] args) throws Exception {
+        HttpCall httpCall = new HttpCall();
+        httpCall.getResponse();
+        httpCall.getResponse2();
     }
 }
--- src/test/java/HttpCallTest.java
+++ src/test/java/HttpCallTest.java
@@ -1,2 +1,32 @@
+import io.keploy.service.mock.MockLib;
+import org.example.HttpCall;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class HttpCallTest {
+    @Test
+    public void testHttpCall() throws Exception {
+        new MockLib("example test 2");
+
+        HttpCall httpCall = new HttpCall();
+        String response = httpCall.getResponse();
+        if (response != null) {
+            assertTrue(true);
+        }
+        assert response != null;
+        assertTrue(response.contains("example"));
+    }
+
+    @Test
+    public void testHttpCall2() throws Exception {
+        new MockLib("google test 2");
+
+        HttpCall httpCall = new HttpCall();
+        String response = httpCall.getResponse2();
+        if (response != null) {
+            assertTrue(true);
+        }
+    }
+
 }
